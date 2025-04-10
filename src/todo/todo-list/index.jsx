753293dd431e1ad450a2c5061020138562207061
Mohamad Aliyul Rahman <aliyulrahman@gmail.com>
2025-04-10 23:22:13 +0530

    added for each

--- src/todo/todo-list/index.jsx
+++ src/todo/todo-list/index.jsx
@@ -6,16 +6,11 @@ function TodoList() {
     return (
         <div className="row">
             <div className="col-md-6 card p-3">
-                <TodoItem {...TODOLISTVALUES[0]}/>
-                <TodoItem {...TODOLISTVALUES[1]}/>
-                <TodoItem {...TODOLISTVALUES[2]}/>
-                <TodoItem {...TODOLISTVALUES[3]}/>
-                <TodoItem {...TODOLISTVALUES[4]}/>
-                <TodoItem {...TODOLISTVALUES[5]}/>
-                <TodoItem {...TODOLISTVALUES[6]}/>
-                <TodoItem {...TODOLISTVALUES[7]}/>
-                <TodoItem {...TODOLISTVALUES[8]}/>
-                <TodoItem {...TODOLISTVALUES[9]}/>
+                {
+                    TODOLISTVALUES.map((item)=> (
+                        <TodoItem key={item.name} {...item}/>
+                    ))
+                }
             </div>
         </div>
     );
